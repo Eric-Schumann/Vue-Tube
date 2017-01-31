4358920c38e772a716a4565b270646633861ea08
Eric.Schumann <eric.schumann82@gmail.com>
2017-01-31 10:53:04 -0600

    Added Test.vue component and imported this component into Hello.vue.

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -17,16 +17,22 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+    <test></test>
   </div>
 </template>
 
 <script>
+import Test from './Test'
+
 export default {
   name: 'hello',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  components: {
+    Test
   }
 }
 </script>
---
+++ src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class=test>
+    <h1>{{test}}</h1>
+    <p>This is a test components written by Eric</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      test: 'This is a test'
+    }
+  }
+}
+</script>
